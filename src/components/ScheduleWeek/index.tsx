0996f59cd8537e1428c63292f6f4c49b2e67c350
Amar Kisoensingh <amar.kisoensingh@gmail.com>
2019-04-10 17:41:10 +0200

    Users can now select specific weeks in the week view

--- src/components/ScheduleWeek/index.tsx
+++ src/components/ScheduleWeek/index.tsx
@@ -37,7 +37,7 @@ class ScheduleWeek extends React.Component<Props, State> {
     thursday: [],
     friday: [],
     loading: true,
-    currentDate: new Date()
+    currentDate: new Date(),
   };
 
   componentDidMount = () => {
@@ -45,7 +45,7 @@ class ScheduleWeek extends React.Component<Props, State> {
   }
 
   getDate = (date: Date) => {
-    let dates: Array<string> = this.getWeekDates(this.state.currentDate)
+    let dates: Array<string> = this.getWeekDates(date)
     let url: string = `https://cors-anywhere.herokuapp.com/${api}${this.props.code}&type=${this.props.type}&startDate=${dates[0]}&endDate=${dates[1]}&json`
     fetch(url, {headers: {'Origin': '',}}).then(res => res.json())
       .then(data => {
@@ -142,9 +142,19 @@ class ScheduleWeek extends React.Component<Props, State> {
 
   getWeekDates = (date: Date) => {
     let weekday: number = date.getDay()
-    let startDate: string = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - (weekday - 1)}`;
-    let endDate: string = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate() - (weekday - 5)}`;
-    return [startDate, endDate];
+    let startDate: Date = new Date(date.getFullYear(), date.getMonth(), date.getDate() - (weekday - 1));
+    let endDate: Date = new Date(date.getFullYear(), date.getMonth(), date.getDate() - (weekday - 5));
+    let startDateString: string = `${startDate.getFullYear()}-${startDate.getMonth() + 1}-${startDate.getDate()}`;
+    let endDateString: string = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}`;
+    return [startDateString, endDateString];
+  }
+
+  getWeekNumbers = (date: Date, pos: number) => {
+    let weekday: number = date.getDay()
+    let newDate: Date = new Date(date.getFullYear(), date.getMonth(), (date.getDate() - (weekday - 1)) + pos)
+    return (
+      <div className={`ScheduleWeek--week-view-header-date ${newDate.getFullYear() == new Date().getFullYear() && newDate.getMonth() == new Date().getMonth() && newDate.getDate() == new Date().getDate() ? 'ScheduleWeek--week-view-header-date-current' : ''}`}>{newDate.getDate()}</div>
+    )
   }
 
   getRandomEmoji = () => {
@@ -179,7 +189,7 @@ class ScheduleWeek extends React.Component<Props, State> {
         }
         res.push(
           <div
-            key={week[i][j].LSID + week[i][j].StartTime}
+            key={week[i][j].LSID + week[i][j].Date}
             className={`
               ScheduleWeek--week-view-item sw-d${i + 1}
               ${new Date(week[i][j].Date).getDate() == new Date().getDate() ? this.isCurrentLesson(week[i][j].StartTime, week[i][j].EndTime) : ''}
@@ -191,10 +201,24 @@ class ScheduleWeek extends React.Component<Props, State> {
               gridRowEnd: parseInt(week[i][j].PeriodEnd) + 2,
             }}
           >
-            <span className="ScheduleWeek--week-view-item-subject">{week[i][j].Subject} {same > 1 ? `+${same}` : null}</span>
-            {/* <span className="ScheduleWeek--week-view-item-text">{week[i][j].Text}</span> */}
-            <span className="ScheduleWeek--week-view-item-class-teacher">{week[i][j].Class} - {week[i][j].Teacher}</span>
-            <span className="ScheduleWeek--week-view-item-room">{week[i][j].room.code}</span>
+            {week[i][j].Subject != null ? (
+              <span className="ScheduleWeek--week-view-item-subject">{week[i][j].Subject} {same > 1 ? `+${same}` : null}</span>
+            ) : null}
+            {/* {week[i][j].Text != null ? (
+              <span className="ScheduleWeek--week-view-item-text">{week[i][j].Text}</span>
+            ) : null} */}
+            <span className="ScheduleWeek--week-view-item-class-teacher">
+              {week[i][j].Class != null ? (
+                week[i][j].Class
+              ) : null}
+              {week[i][j].Teacher != null ? (
+                ` - ${week[i][j].Teacher}`
+              ) : null}
+            </span>
+            {/* <span className="ScheduleWeek--week-view-item-class-teacher">{week[i][j].Class} - {week[i][j].Teacher}</span> */}
+            {week[i][j].room != null ? (
+              <span className="ScheduleWeek--week-view-item-room">{week[i][j].room.code}</span>
+            ) : null }
           </div>
         )
       }
@@ -219,48 +243,68 @@ class ScheduleWeek extends React.Component<Props, State> {
   }
 
   handleDateChange = (date: any) => {
-    console.log(date)
     this.setState({
-      currentDate: date
+      currentDate: new Date(date),
+      loading: true,
     })
+    this.getDate(new Date(date))
+  }
+
+  handleDateChangePrev = () => {
+    let newDate: Date = new Date(this.state.currentDate.getFullYear(), this.state.currentDate.getMonth(), this.state.currentDate.getDate() - 7);
+    this.setState({
+      currentDate: newDate,
+      loading: true,
+    })
+    this.getDate(newDate)
+  }
+
+  handleDateChangeNext = () => {
+    let newDate: Date = new Date(this.state.currentDate.getFullYear(), this.state.currentDate.getMonth(), this.state.currentDate.getDate() + 7);
+    this.setState({
+      currentDate: newDate,
+      loading: true,
+    })
+    this.getDate(newDate)
   }
 
   public render() {
     return (
       <div className="ScheduleWeek--container">
         <div className="ScheduleWeek--top-bar">
-          <Button className="ScheduleWeek--previous-week-button"><KeyboardArrowLeft /></Button>
+          <Button className="ScheduleWeek--previous-week-button" onClick={this.handleDateChangePrev} disabled={this.state.loading}><KeyboardArrowLeft /></Button>
           <InlineDatePicker
             onlyCalendar
             variant="outlined"
             className="ScheduleWeek--current-date-button"
             value={this.state.currentDate}
             onChange={this.handleDateChange}
+            disabled={this.state.loading}
           />
-          <Button className="ScheduleWeek--next-week-button"><KeyboardArrowRight /></Button>
+          <Button className="ScheduleWeek--next-week-button" onClick={this.handleDateChangeNext} disabled={this.state.loading}><KeyboardArrowRight /></Button>
         </div>
         <div className="ScheduleWeek--week-view-container">
           {this.getSchedBorders()}
           <div className="ScheduleWeek--week-view-header sw-empty"></div>
           <div className="ScheduleWeek--week-view-header sw-day1">
             <div className="ScheduleWeek--week-view-header-day">M</div>
-            <div className="ScheduleWeek--week-view-header-date ScheduleWeek--week-view-header-date-current">12</div>
+            {this.getWeekNumbers(this.state.currentDate, 0)}
           </div>
           <div className="ScheduleWeek--week-view-header sw-day2">
             <div className="ScheduleWeek--week-view-header-day">T</div>
-            <div className="ScheduleWeek--week-view-header-date">1</div>
+            {this.getWeekNumbers(this.state.currentDate, 1)}
           </div>
           <div className="ScheduleWeek--week-view-header sw-day3">
             <div className="ScheduleWeek--week-view-header-day">W</div>
-            <div className="ScheduleWeek--week-view-header-date">1</div>
+            {this.getWeekNumbers(this.state.currentDate, 2)}
           </div>
           <div className="ScheduleWeek--week-view-header sw-day4">
             <div className="ScheduleWeek--week-view-header-day">T</div>
-            <div className="ScheduleWeek--week-view-header-date">1</div>
+            {this.getWeekNumbers(this.state.currentDate, 3)}
           </div>
           <div className="ScheduleWeek--week-view-header sw-day5">
             <div className="ScheduleWeek--week-view-header-day">F</div>
-            <div className="ScheduleWeek--week-view-header-date">1</div>
+            {this.getWeekNumbers(this.state.currentDate, 4)}
           </div>
           <div className="ScheduleWeek--week-view-time-container sw-c0-p1">
             1
